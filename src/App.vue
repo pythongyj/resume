1942e240696663f18258370df5fd47bb42d59533
YunjieGe <1329712473@qq.com>
2020-02-10 08:48:33 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <div id="nav" >
+      <router-link to="/">个人简历</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+   background-color: #008c8c;
+}
+  *{
+    margin: 0;
+    padding: 0;
+  }
+  .router-link-exact-active{
+    text-decoration: none;
+  }
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="home" v-title data-title="简历">
+    <div class="container">
+      <div class="center">
+        <div class="title">
+          <h2>{{title}}</h2>
+          414652f8672ed787e3aef692c08119900e9e4872
+          <h2>看看可以了不</h2>
+        </div>
+      </div>
+      <br>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {},
+  data () {
+    return {
+      title: '3年web前端工程师 葛运杰'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .home{
+    width: 100%;
+    height: 100vh;
+    background-color: #008c8c;
+    .container{
+      width: 1200px;
+      height: 90vh;
+      background-color: #fff;
+      margin: 0 auto;
+      .center{
+        width: 1080px;
+        background-color: aqua;
+        margin: 0 auto;
+        border-bottom: 3px solid #666;
+        .title{
+          width: 100%;
+          overflow: hidden;
+          h2{
+            font-size: 28px;
+            color: #333;
+            font-family: '微软雅黑';
+            margin: 20px 0;
+          }
+        }
+      }
+    }
+  }
+</style>
